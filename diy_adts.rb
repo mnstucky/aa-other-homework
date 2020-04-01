e0e95134f5b8e85a98064645953524372257482b
Matt Stucky <mnstucky@gmail.com>
2020-04-01 09:24:11 -0500

    Add custom queque class

--- diy_adts.rb
+++ diy_adts.rb
@@ -24,12 +24,50 @@ class Stack
 end
 
 #code for testing 
-s = Stack.new
-p s.stack
-s.push(1)
-s.push(2)
-s.push(3)
-p s.stack
-p s.peek
-s.pop
-p s.stack
+# s = Stack.new
+# p s.stack
+# s.push(1)
+# s.push(2)
+# s.push(3)
+# p s.stack
+# p s.peek
+# s.pop
+# p s.stack
+
+class Queue
+
+    attr_reader :queue
+    
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        # adds an element to the queue
+        @queue << el
+    end
+
+    def dequeue
+        # removes one element from the queue
+        @queue.delete_at(0)
+    end
+
+    def peek
+        @queue[0]
+    end
+
+end
+
+#code for testing
+# q = Queue.new
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+# p q.queue
+# p q.peek
+# q.dequeue
+# p q.queue
+# q.dequeue
+# p q.queue
+
+
